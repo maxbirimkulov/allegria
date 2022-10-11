a9497d2e8259308c2814317beec0198be4f42c91
maxbirimkulov <maxbirimkulov@mail.ru>
2022-10-11 20:35:07 +0600

    add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React , {useState} from "react";
-import Header from "./Layout/Header/Header";
-import Footer from "./Layout/Footer/Footer";
+import React , {useState, Suspense} from "react";
 import Home from "./pages/Home/Home";
 import './scss/style.scss'
 import Layout from "./Layout/Layout";
+import About from "./pages/About/About";
+import Catalog from "./pages/Catalog/Catalog";
+import {Route, Routes} from 'react-router-dom'
+import Custom from "./pages/Custom/Custom";
+import NotFound from "./pages/NotFound/NotFound";
+import './i18n'
 
 function App() {
   return (
-    <>
-      <Layout/>
-    </>
+    <Suspense fallback={"...loading"}>
+        <Routes>
+            <Route path='/' element={<Layout/>}>
+                <Route path='' element={<Home/>}/>
+                <Route path='about' element={<About/>}/>
+                <Route path='catalog' element={<Catalog/>}/>
+                <Route path='*' element={<NotFound/>}/>
+            </Route>
+            <Route path='/custom' element={<Custom/>}/>
+        </Routes>
+
+    </Suspense>
   );
 }
 
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from "./Header/Header";
-import Home from "../pages/Home/Home";
 import Footer from "./Footer/Footer";
+import {Outlet} from 'react-router-dom'
 
 const Layout = () => {
     return (
         <>
             <Header/>
-            <Home/>
+            <Outlet/>
             <Footer/>
         </>
     );
