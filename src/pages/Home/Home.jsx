20432b7eaa5a8546d6194a3ffd7efe4bb1d7f999
Phantather <akdil.hstech@gmail.com>
2022-10-11 15:51:49 +0600

    add sections: popular & about in home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Begin from "./Begin/Begin";
 import Collection from "./Collection/Collection";
+import Popular from "./Popular/Popular";
+import About from "./About";
 
 const Home = () => {
     return (
         <main>
             <Begin/>
             <Collection/>
+            <Popular/>
+            <About/>
         </main>
     );
 };
